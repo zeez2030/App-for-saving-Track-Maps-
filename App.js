0c7d9f2e58daeb2be7c2dc9fa05c80e9c3b9b6cf
zeez <zeezrdq@gmail.com>
2019-08-20 03:08:30 +0200

    finished the screens layout and navigation

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  createAppContainer,
+  createStackNavigator,
+  createBottomTabNavigator,
+  createSwitchNavigator
+} from "react-navigation";
+
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen
+    }),
+    CreateTrack: TrackCreateScreen,
+    Account: AccountScreen
+  })
+});
+
+export default createAppContainer(switchNavigator);
---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+
+const SigninScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>Sign in Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SigninScreen;
